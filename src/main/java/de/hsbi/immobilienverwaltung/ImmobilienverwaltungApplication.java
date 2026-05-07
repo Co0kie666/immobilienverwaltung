c5d95676a2996fb28a873f0e43400cd9cca6aa3f
Jannick Born <Jannick-born@t-online.de>
2026-05-07 17:22:11 +0200

    Initialization

---
+++ src/main/java/de/hsbi/immobilienverwaltung/ImmobilienverwaltungApplication.java
@@ -0,0 +1,12 @@
+package de.hsbi.immobilienverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilienverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmobilienverwaltungApplication.class, args);
+    }
+}
