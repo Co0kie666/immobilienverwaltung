fde8e2e1ccbf11b773837f11cb9cd448a1aed1db
samuelmaier7 <samremi11@gmail.com>
2026-05-19 18:59:53 +0200

    sql file erstellt

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO immobilie (bezeichnung, typ, baujahr)
+VALUES
+    ('Parkresidenz Süd', 'Mehrfamilienhaus', 1998),
+    ('Altbau Ensemble Mitte', 'Wohngebäude', 1965),
+    ('Seeblick Quartier', 'Gewerbeimmobilie', 2012);
