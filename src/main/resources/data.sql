8e381b649167f54597ae44a7304fea028c6c9c13
samuelmaier7 <samremi11@gmail.com>
2026-05-24 16:07:47 +0200

    data.sql angepasst

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,10 @@
-INSERT INTO adresse (id, strasse, hausnummer, plz, stadt)
+INSERT INTO immobilie (bezeichnung, typ, baujahr, flaeche, strasse, hausnummer, plz, stadt)
 VALUES
-    (1, 'Parkstraße', '12', '10115', 'Berlin'),
-    (2, 'Hauptstraße', '10', '80331', 'München'),
-    (3, 'Seestraße', '8-10', '20099', 'Hamburg');
+    ('Parkresidenz Süd', 'MEHRFAMILIENHAUS', 1998, 850,
+     'Parkstraße', '43', '33605', 'Bielefeld'),
 
-INSERT INTO immobilie (id, bezeichnung, typ, baujahr, flaeche, adresse_id)
-VALUES
-    (1, 'Parkresidenz Süd', 'MEHRFAMILIENHAUS', 1998, 850, 1),
-    (2, 'Altbau Ensemble Mitte', 'WOHNGEBAEUDE', 1965, 850, 2),
-    (3, 'Seeblick Quartier', 'GEWERBEIMMOBILIE', 2012, 850, 3);
+    ('Altbau Ensemble Mitte', 'WOHNGEBAEUDE', 1965, 850,
+     'Apfelstraße', '1', '33602', 'Bielefeld'),
 
---- damit H2 Datenbank neue IDs ab nummer 4 vergibt ---
-ALTER TABLE adresse ALTER COLUMN id RESTART WITH 4;
-ALTER TABLE immobilie ALTER COLUMN id RESTART WITH 4;
+    ('Seeblick Quartier', 'GEWERBEIMMOBILIE', 2012, 850,
+     'Seestraße', '21', '33613', 'Bielefeld');
