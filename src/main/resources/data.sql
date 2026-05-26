1cb40283cc56fe14dde671dd436a5ea71ea2bb66
Jannick Born <Jannick-born@t-online.de>
2026-05-26 19:07:20 +0200

    Testdaten angelegt für Mieteinheit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,3 +17,10 @@ VALUES (
            '$2a$10$3W7jwY/gaIDV/LzwG1vJie9tZfeDwk2lMtXhlnXwI.IvM5mJoOZRC'
     --passwort: test
        );
+
+INSERT INTO mieteinheit (groesse, zimmerzahl, immobilie_id, bezeichnung, stockwerk, status, typ)
+VALUES
+    (50, 3, 3, 'Testbüro 1', 1, 'FREI', 'BUERO'),
+    (55, 2, 3, 'Testbüro 2', 1, 'FREI', 'BUERO'),
+    (70, 4, 3, 'Testbüro 3', 2, 'VERMIETET', 'BUERO'),
+    (45, 1, 3, 'Testbüro 4', 3, 'IN_RENOVIERUNG', 'BUERO');
