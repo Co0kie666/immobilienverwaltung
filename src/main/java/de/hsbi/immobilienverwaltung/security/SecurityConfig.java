6650594bb98ab40d4f2f6c70f4bb1c4995b55181
Ali Mabrouk <alimabrouk801@gmail.com>
2026-06-05 17:10:29 +0200

    Buchunhsübersicht ist jz verbunden. security minor Änderung damit man zugriff auf datenbank hat . Test Values für Ausgabe sind da Anzeigen und bearbeiten sind noch nicht verbunden brauche noch ein Ui extra brauche noch Zeit

--- src/main/java/de/hsbi/immobilienverwaltung/security/SecurityConfig.java
+++ src/main/java/de/hsbi/immobilienverwaltung/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import de.hsbi.immobilienverwaltung.ui.auth.LoginView;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/h2-console/**").permitAll()
+        );
+
+        http.csrf(csrf -> csrf
+                .ignoringRequestMatchers("/h2-console/**")
+        );
+
+        http.headers(headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+        );
+
         return http.with(
                 VaadinSecurityConfigurer.vaadin(),
                 configurer -> configurer.loginView(LoginView.class)
